c3b24b5421d76c16cb076b237c5321db2c7dc1eb
L <freringl@gmail.com>
2024-02-10 20:17:19 +0100

    basic swarm working

--- scripts/ant.gd
+++ scripts/ant.gd
@@ -4,11 +4,15 @@ extends CharacterBody2D
 
 var neighbors
 var neighbors_directions
-const N = 6
-var sigma_repulsion = 1.0
+const N = 10
+var sigma_repulsion = 4.6
 var weight_repulsion = 1.0
+var weight_cohesion = 2.0
+var weight_player = 1.0
 
 var f_r
+var f_c
+var f_p
 var total_force
 var new_velocity
 
@@ -46,28 +50,58 @@ func compute_neighbors_directions():
 
 func compute_repulsive_force():
 	'''
-	Computes a force between 0 and 1 that goes in the opposite direction as the
+	Computes a force that goes in the opposite direction as the
 	neighbors' average
 	'''
 	var f
-	var s = Vector2(0,0)
+	var s = Vector2(0.0, 0.0)
 	var dir
 	var neighbor
 	
 	for neighbor_index in len(neighbors):
 		dir = neighbors_directions[neighbor_index]
 		neighbor = neighbors[neighbor_index]
-		print(-self.position.distance_to(neighbor.position))
 		s = s + dir*exp(-self.position.distance_to(neighbor.position)/sigma_repulsion**2)
 	f = - weight_repulsion * s / N
-	print(f)
+	return f
+	
+func compute_cohesive_force():
+	'''
+	Computes a force that goes towards neighbors'
+	center of gravity
+	'''
+	var f
+	var s = Vector2(0.0, 0.0)
+	var dir
+	for neighbor_index in len(neighbors):
+		dir = neighbors_directions[neighbor_index]
+		s = s + dir
+	f = weight_cohesion * s / N
+	return f
+
+func compute_player_force():
+	'''
+	Computes a force that goes towards player input direction
+	'''
+	var f = Vector2()
+	if Input.is_action_pressed("ui_left"):
+		f.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		f.x += 1
+	if Input.is_action_pressed("ui_up"):
+		f.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		f.y += 1
+	f = weight_player * f
 	return f
 
 func _physics_process(delta):
 	find_neighbors()
 	compute_neighbors_directions()
 	f_r = compute_repulsive_force()
-	total_force = 100*f_r
+	f_c = compute_cohesive_force()
+	f_p = compute_player_force()
+	total_force = 10*(f_r + f_c + f_p)
 	new_velocity = get_velocity() + (1/1.0)*total_force*delta
 	set_velocity(new_velocity)
 	move_and_slide()
