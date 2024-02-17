47fb6724220d082a4f2b27c8691002c45623959f
Viven Fargette <vivien.f@live.fr>
2024-02-17 20:02:11 +0100

    kill an ant when outside of pheromone for too long

--- scripts/mainAnt.gd
+++ scripts/mainAnt.gd
@@ -1,16 +1,33 @@
 extends CharacterBody2D
 
 @onready var tilemap : TileMap = get_tree().get_nodes_in_group("Tilemap")[0]
+@onready var ants = get_tree().get_nodes_in_group("ants")
+@export var timeBeforeDeathOutsidePheromone : float = 10
 
 var speed : float = 150
 var pheromoneLayer : int = 0
+var timer : float = 0
+var IsOnPheromone = "IsOnPheromone"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 	
 func _process(_delta : float):
-	var tile_pos = tilemap.local_to_map(self.global_position)
+	var tile_pos : Vector2i = tilemap.local_to_map(self.global_position)
+	var tile_data : TileData = tilemap.get_cell_tile_data(pheromoneLayer,tile_pos)
+	if(tile_data):
+		var isOnPheromone = tile_data.get_custom_data(IsOnPheromone)
+		if(!isOnPheromone):
+			timer+=_delta
+			if(timer>timeBeforeDeathOutsidePheromone):
+				ants = get_tree().get_nodes_in_group("ants")
+				var index = randi()%len(ants)
+				ants[index].queue_free()
+				timer = 0
+		else:
+			timer = 0
+
 	
 func compute_player_direction() -> Vector2:
 	'''
@@ -35,5 +52,4 @@ func _physics_process(_delta):
 	newSpeed.x *= speed
 	newSpeed.y *= speed
 	self.velocity = newSpeed
-	var move = move_and_slide()
-	print(move)
+	move_and_slide()
